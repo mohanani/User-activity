a729769b9aaaccf8c0b0cc39c6228db86d7fcd7f
Mohan Kakarla <mohan.nani88@gmail.com>
2020-07-31 16:02:39 +0530

    Add files via upload

---
+++ frontend/build/precache-manifest.dfc860f58fb49cf95ee4afddd0f84d93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "283fc9b71850b6fcadf93ee88e0e3133",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e6700ecd27c918ab2ac",
+    "url": "/static/css/2.63343480.chunk.css"
+  },
+  {
+    "revision": "3366a97251c6edf53216",
+    "url": "/static/css/main.b37fbcb3.chunk.css"
+  },
+  {
+    "revision": "6e6700ecd27c918ab2ac",
+    "url": "/static/js/2.befe84c6.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/static/js/2.befe84c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3366a97251c6edf53216",
+    "url": "/static/js/main.3bbfca29.chunk.js"
+  },
+  {
+    "revision": "731d496cac4c8086fb49",
+    "url": "/static/js/runtime-main.bc3cae67.js"
+  }
+]);
